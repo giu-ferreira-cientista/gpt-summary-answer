3158aa0ec0e0a3556c826fa21e797d31405a43ba
giu-ferreira-cientista <giuferreira@gmail.com>
2023-03-26 18:12:52 +0000

    Interface Improvements

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,6 +3,7 @@ from requests_html import HTMLSession
 import matplotlib.pyplot as plt
 import pandas as pd
 import time
+from PIL import Image
 
 SITE_URL = "https://dbd.puc-rio.br/TecnicasAvancadasNLP.html"
 API_URL = "https://8080-giuferreira-gptsummarya-kxk348b5zkl.ws-us92.gitpod.io"
@@ -15,9 +16,11 @@ def get_data(url, question):
     print(course_json)
     return course_json
 
-st.title('Stack Academy - Assistente de Cursos')
-st.markdown("## Consulta")
-question = st.text_input("O que deseja saber?", "Do que se trata esse curso?")
+image = Image.open('./images/logo.jpg')
+st.image(image, width=100)
+st.title('Stack Academy')
+st.title('Assistente de Cursos')
+question = st.text_input("O que deseja saber?", "Do que se trata esse curso?", )
 if st.button("Run"):        
     with st.spinner('Mágica em andamento...'):
         answer = get_data(url=SITE_URL, question=question)
@@ -27,4 +30,4 @@ if st.button("Run"):
     t = st.empty()
     for i in range(len(text) + 1):
         t.markdown("## %s" % text[0:i])
-        time.sleep(0.06)
+        time.sleep(0.04)
